383cd4be8f6b237e454a1f66453a497a84198c08
Andreas Pfeiffer <apfeiffer1@gmail.com>
2016-05-20 23:33:32 +0200

    add postgres db writer

---
+++ outputs/airPiModels.py
@@ -0,0 +1,63 @@
+import netrc
+
+import datetime
+
+import sqlalchemy as sa
+
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.dialects.postgresql import JSONB
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# Time: 2015-08-14 08:31:04.336307
+# Temperature: 37.1 C
+# Pressure: 959.47 hPa
+# Relative_Humidity: 30.0 %
+# Light_Level: 417.515274949 Ohms
+# Nitrogen_Dioxide: 13790.6976744 Ohms
+# Carbon_Monoxide: 127333.333333 Ohms
+# Volume: 716.129032258 mV
+
+class AirPiData(Base):
+    __tablename__ = 'airPiData'
+
+    id0                = Column(Integer, primary_key=True)
+    timeStamp          = Column(DateTime())
+    Temperature        = Column(Float())
+    Pressure           = Column(Float())
+    Relative_Humidity  = Column(Float())
+    Light_Level        = Column(Float())
+    Nitrogen_Dioxide   = Column(Float())
+    Carbon_Monoxide    = Column(Float())
+    Volume             = Column(Float())
+
+    def __init__(self, temp, pres, relH, light, no2, co, vol):
+        self.timeStamp = datetime.datetime.now()
+        self.Temperature        = temp
+        self.Pressure           = pres
+        self.Relative_Humidity  = relH
+        self.Light_Level        = light
+        self.Nitrogen_Dioxide   = no2
+        self.Carbon_Monoxide    = co
+        self.Volume             = vol
+
+    def __repr__(self):
+      return "<AirPiData instance: %s>\n" % self.__dict__
+
+
+user,login,pwd = netrc.netrc().authenticators('postgres')
+engine = create_engine('postgresql://pi:%s@rpi2-0.local/airpi' % pwd, echo=False)
+session = sessionmaker(bind=engine)
+
+session.configure(bind=engine)
+
+Base.metadata.create_all(engine)
---
+++ outputs/pgWriter.py
@@ -0,0 +1,69 @@
+import output
+import datetime
+
+from airPiModels import *
+
+# Time: 2015-08-14 08:31:04.336307
+# Temperature: 37.1 C
+# Pressure: 959.47 hPa
+# Relative_Humidity: 30.0 %
+# Light_Level: 417.515274949 Ohms
+# Nitrogen_Dioxide: 13790.6976744 Ohms
+# Carbon_Monoxide: 127333.333333 Ohms
+# Volume: 716.129032258 mV
+
+dType = { 'Time' : 'datetime',
+          'Temperature' : 'float',
+          'Pressure' : 'float', 
+          'Relative_Humidity' : 'float', 
+          'Light_Level' : 'float', 
+          'Nitrogen_Dioxide' : 'float', 
+          'Carbon_Monoxide' : 'float', 
+          'Volume' : 'float', 
+         }
+
+dTypeList = [ 'Time', 'Temperature', 'Pressure', 'Relative_Humidity', 'Light_Level', 'Nitrogen_Dioxide', 'Carbon_Monoxide', 'Volume', ]
+
+class PGWriter(output.Output):
+
+    # requiredData = ["dbName"]
+    optionalData = ["dbName"]
+    optionalData = ["tableName"]
+    def __init__(self,data):
+       self.dbName='airpi'
+       if data.has_key('dbName'): self.tableName = data['dbName']
+       self.tableName = 'airPiData'
+       if data.has_key('tableName'): self.tableName = data['tableName']
+
+    def outputData(self,dataPoints):
+
+        # print "datapoints:", str(dataPoints)
+
+        # we need the data sorted by the keys of dTypeList
+        map = { "Time" : datetime.datetime.now() }
+        for i in dataPoints:
+           map[ i['name'] ] = i["avg"]
+
+        # check if we got everything, otherwise add dummy values
+        for k in dTypeList:
+            if map.has_key(k): continue
+            print "no item found for %s in datapoints" % k
+            map[k] = -9999.
+
+        try:
+            sess = session()
+            apd = AirPiData(temp = map['Temperature'], 
+                            pres = map['Pressure'], 
+                            relH = map['Relative_Humidity'], 
+                            light = map['Light_Level'], 
+                            no2 = map['Nitrogen_Dioxide'], 
+                            co = map['Carbon_Monoxide'], 
+                            vol = map['Volume'], 
+                            )
+            sess.add(apd)
+            sess.commit()
+        except Exception, e:
+            print "ERROR when uploading to Postgres DB: ", str(e)
+            return
+
+
